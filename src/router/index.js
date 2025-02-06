51726f73e72df08ccc74c63d87be8d8a38485ab0
AlfredoPC <alfredo.galvez2506@gmail.com>
2025-02-05 21:36:07 -0700

    commit test v2 deploy

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory('/AirBnb/Frontend/dist/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory('/AirBnb/Frontend/dist/'),
   routes: [
     {
       path: '/',
