7a6b8f6b5dc5e652e81b2559a0c028daf79f4f51
AlfredoPC <alfredo.galvez2506@gmail.com>
2025-02-05 21:06:18 -0700

    commit code

---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-if="loading">Cargando usuarios...</div>
+    <li v-else v-for="character in characters" :key="character.id">
+      {{ character.name }}
+    </li>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const characters = ref([])
+    const loading = ref(true)
+
+    onMounted(async () => {
+      try {
+        const response = await fetch('http://localhost:2506/api/v1/users')
+        const data = await response.json()
+        characters.value = data
+      } catch (error) {
+        console.error('Error al cargar personajes:', error)
+      } finally {
+        setTimeout(() => {
+          loading.value = false
+        }, 3000);
+      }
+    })
+
+    return {
+      characters,
+      loading
+    }
+  }
+}
+</script>
